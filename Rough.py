abff82a777e86162bfec33e75ccc376583b23152
Arnav <arnavjindal24@gmail.com>
2020-12-09 14:19:44 +0530

    another practice problem added

--- Rough.py
+++ Rough.py
@@ -23,4 +23,3 @@
 # print (width)
 # for i in range(1,n+1):
 #     print ("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
-
--- practiceProblem-4.py
+++ practiceProblem-4.py
@@ -1,3 +1,13 @@
+'''
+                                                                                            'Learn from Mistakes'
+
+
+Author: Arnav
+Date: 8 Dec 2020
+Purpose: Practice problem
+'''
+
+
 # “The Next Palindrome”
 # Problem Statement:-
 # A palindrome is a string that, when reversed, is equal to itself. Example of the palindrome includes:
@@ -23,7 +33,7 @@
 # Next palindrome for 2311 is 2222
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
 counter =0
-n = int(input("Enter number of test cases: "))
+n = int(input("Enter number of test cases: \n"))
 # nums = [i for i in input("Enter numbers separated by ',' ").split(",")]
 num = []
 
@@ -45,12 +55,42 @@ for k in num:
             if lis[i] == lis[-(i+1)]:
 
                 if i == (int(((len(lis)+1)/2)-1)):
-                    print(f"Next palindrome is {palin}")
+                    print(f"Next palindrome is {palin}\n")
                     breaker =1
                 else:
                     continue
 
             else:
                 break
-
         counter += 1
+
+# really poor execution of program
+# I forgot to use the three methods he taught in last problem (I picked the hardest Method)
+# CWH's execution (Pretty neat):
+'''
+Author: Harry
+Date: 15 April 2019
+Purpose: Practice Problem For CodeWithHarry Channel 
+'''
+
+
+def next_palindrome(n):
+    n = n+1
+    while not is_palindrome(n):
+        n += 1
+    return n
+
+
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+
+if __name__ == "__main__":
+    n = int(input("Enter the number of test cases\n"))
+    numbers = []
+    for i in range(n):
+        number = int(input("Enter the number:\n"))
+        numbers.append(number)
+
+    for i in range(n):
+        print(f"Next palindrome for {numbers[i]} is {next_palindrome(numbers[i])}")
